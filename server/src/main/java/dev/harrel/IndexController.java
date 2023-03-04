7fd2c850811f8ef2e495852641e143069797d377
Harrel <marcin.slowik95@gmail.com>
2023-03-04 16:18:58 +0100

    try version controller

--- server/src/main/java/dev/harrel/IndexController.java
+++ server/src/main/java/dev/harrel/IndexController.java
@@ -6,11 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    public IndexController() {
-        System.out.println("IndexController");
-    }
-
-    @GetMapping("/test")
+    @GetMapping
     public String test() {
         return "index.html";
     }
