e989333944c5aebd8823133d996bc72b4defad9a
Harrel <marcin.slowik95@gmail.com>
2023-03-04 17:28:18 +0100

    version using thymeleaf

--- server/src/main/java/dev/harrel/IndexController.java
+++ server/src/main/java/dev/harrel/IndexController.java
@@ -1,13 +1,25 @@
 package dev.harrel;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
+    private final String version;
+
+    public IndexController(@Value("${TAG:unspecified}") String version) {
+        this.version = version;
+    }
+
     @GetMapping
-    public String test() {
-        return "index.html";
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("index");
+        mav.addObject("version", version);
+        return mav;
     }
 }
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 
+const version = document.querySelector('meta[name="version"]')?.getAttribute('content')
+
 const App = () => {
-    return <div className="main-container">Hello there</div>;
+    return (
+        <div className='main-container'>
+            <div className='content-container'>Hi</div>
+            <div className='footer-container'>
+                <p>{version}</p>
+            </div>
+        </div>);
 }
 
 const root = ReactDOMClient.createRoot(document.getElementById('root')!);
