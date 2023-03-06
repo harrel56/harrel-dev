611605b3fe0d207ee77e9b7ce7f450e428f76de9
Harrel <marcin.slowik95@gmail.com>
2023-03-06 23:17:13 +0100

    spring to javalin

--- server/src/main/java/dev/harrel/App.java
+++ server/src/main/java/dev/harrel/App.java
@@ -1,12 +1,32 @@
 package dev.harrel;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
+import io.javalin.Javalin;
+import io.javalin.config.JavalinConfig;
+import io.javalin.rendering.template.JavalinJte;
 
-@SpringBootApplication
+import java.util.Map;
+import java.util.function.Consumer;
+
+import static ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME;
+
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        Logger root = (Logger) org.slf4j.LoggerFactory.getLogger(ROOT_LOGGER_NAME);
+        root.setLevel(Level.INFO);
+
+        TemplateEngine templateEngine = TemplateEngine.create(new ResourceCodeResolver("templates"), ContentType.Html);
+        JavalinJte.init(templateEngine);
+
+        String version = System.getProperty("TAG", "unspecified");
+        Consumer<JavalinConfig> configConsumer = config -> config.staticFiles.add("/static");
+        Javalin.create(configConsumer)
+                .get("/", ctx -> ctx.render("index.jte", Map.of("version", version)))
+                .start(8080);
     }
 }
