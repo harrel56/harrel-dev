7d0d6d1215add6d3153fec8cee6ad127a23fa85a
Harrel <marcin.slowik95@gmail.com>
2023-06-29 00:53:05 +0200

    json-schema description + validation messages

--- server/src/main/java/dev/harrel/ValidationHandler.java
+++ server/src/main/java/dev/harrel/ValidationHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.harrel.jsonschema.Error;
+import dev.harrel.jsonschema.InvalidSchemaException;
 import dev.harrel.jsonschema.Validator;
 import dev.harrel.jsonschema.ValidatorFactory;
 import io.javalin.http.Context;
@@ -13,14 +14,14 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
 
 public class ValidationHandler implements Handler {
     private static final Logger logger = LoggerFactory.getLogger(ValidationHandler.class);
 
     private final ObjectMapper mapper = new ObjectMapper()
             .addMixIn(Error.class, ErrorMixIn.class);
-    private final ValidatorFactory factory = new ValidatorFactory()
-            .withDefaultMetaSchemaUri(null);
+    private final ValidatorFactory factory = new ValidatorFactory();
 
     @Override
     public void handle(Context ctx) throws Exception {
@@ -28,12 +29,21 @@ public class ValidationHandler implements Handler {
         String body = ctx.body();
         logger.info("Validation request: {}", body);
         JsonNode jsonNode = mapper.readTree(body);
-        Validator.Result result = factory.validate(jsonNode.get("schema"), jsonNode.get("instance"));
-        String errorsJson = mapper.writeValueAsString(result.getErrors());
-        ctx.result(errorsJson);
-        logger.info("Validation result: {}ms {}", Duration.between(startTime, Instant.now()).toMillis(), errorsJson);
+        Response response;
+        try {
+            Validator.Result result = factory.validate(jsonNode.get("schema"), jsonNode.get("instance"));
+            response = new Response(result.isValid(), result.getErrors(), null);
+        } catch (InvalidSchemaException e) {
+            response = new Response(false, e.getErrors(), "Schema failed validation against meta-schema");
+            ctx.status(400);
+        }
+        String responseJson = mapper.writeValueAsString(response);
+        ctx.result(responseJson);
+        logger.info("Validation result: {}ms {}", Duration.between(startTime, Instant.now()).toMillis(), responseJson);
     }
 }
+
+record Response(boolean valid, List<Error> errors, String errorMessage) {}
 
 interface ErrorMixIn {
     @JsonIgnore
--- web/src/JsonSchemaPlayground.tsx
+++ web/src/JsonSchemaPlayground.tsx
@@ -5,76 +5,99 @@ import {Button} from './Button'
 import {linter, lintGutter} from '@codemirror/lint'
 import ReactCodeMirror from '@uiw/react-codemirror'
 
+interface Response {
+  valid: boolean
+  errors: any[]
+  errorMessage?: any
+}
+
 const JsonSchemaPlayground = () => {
-    const [schema, setSchema] = useState('{}')
-    const [instance, setInstance] = useState('{}')
-    const [response, setResponse] = useState<string>()
-    const [parseError, setParseError] = useState<string>()
+  const [schema, setSchema] = useState('{}')
+  const [instance, setInstance] = useState('{}')
+  const [response, setResponse] = useState<Response>()
+  const [parseError, setParseError] = useState<string>()
 
-    const {post, loading, error} = useFetch<any>('/api/json-validate', {cachePolicy: CachePolicies.NO_CACHE})
+  const {post, loading, error} = useFetch<any>('/api/json-validate', {cachePolicy: CachePolicies.NO_CACHE})
 
-    const onClick = async () => {
-        let schemaJson, instanceJson
-        try {
-            schemaJson = JSON.parse(schema)
-            setParseError(undefined)
-        } catch (e) {
-            setParseError('Schema is not a valid JSON')
-            setResponse('')
-
-            return
-        }
-        try {
-            instanceJson = JSON.parse(instance)
-            setParseError(undefined)
-        } catch (e) {
-            setParseError('Instance is not a valid JSON')
-            setResponse('')
-            return
-        }
-        const result = await post({schema: schemaJson, instance: instanceJson})
-        setResponse(JSON.stringify(result, null, 2))
+  const onClick = async () => {
+    let schemaJson, instanceJson
+    try {
+      schemaJson = JSON.parse(schema)
+      setParseError(undefined)
+    } catch (e) {
+      setParseError('Schema is not a valid JSON')
+      setResponse(undefined)
+      return
     }
+    try {
+      instanceJson = JSON.parse(instance)
+      setParseError(undefined)
+    } catch (e) {
+      setParseError('Instance is not a valid JSON')
+      setResponse(undefined)
+      return
+    }
+    const result = await post({schema: schemaJson, instance: instanceJson})
+    setResponse(result)
+  }
 
-    const extensions = useMemo(() => [json(), lintGutter(), linter(jsonParseLinter(), {delay: 300})], [])
-    const errorMessage = parseError ?? error?.message
+  const extensions = useMemo(() => [json(), lintGutter(), linter(jsonParseLinter(), {delay: 300})], [])
+  const errorMessage = parseError ?? response?.errorMessage ?? error?.message
+  let validationResponse = null
+  if (errorMessage) {
+    validationResponse = <p className='error-message'>{errorMessage}</p>
+  } else if (response) {
+    validationResponse = response.valid ? <p className='success-message'>Validation successful</p> : <p className='error-message'>Validation failed</p>
+  }
 
-    return (
-        <>
-            <h1>JSON Schema</h1>
-            <div className='editors-wrapper'>
-                <div className='editor-container'>
-                    <h2>Schema</h2>
-                    <ReactCodeMirror
-                        extensions={extensions}
-                        height='500px'
-                        value={schema}
-                        onChange={val => setSchema(val)}
-                    />
-                </div>
-                <div className='editor-container'>
-                    <h2>Instance</h2>
-                    <ReactCodeMirror
-                        extensions={extensions}
-                        height='500px'
-                        value={instance}
-                        onChange={val => setInstance(val)}
-                    />
-                </div>
-            </div>
-            {errorMessage && <p className='error-message'>{errorMessage}</p>}
-            <Button disabled={loading} onClick={onClick} className='flex-start'>Validate</Button>
-            <div className='editor-container'>
-                <h2>Output</h2>
-                <ReactCodeMirror
-                    extensions={[json()]}
-                    height='500px'
-                    value={response}
-                    readOnly={true}
-                />
-            </div>
-        </>
-    )
+  const output = response ? JSON.stringify(response.errors, null, 2) : ''
+
+  return (
+    <>
+      <h1>JSON Schema</h1>
+      <p>
+        This validator is implemented in Java and supports only <i>draft2020-12</i> specification version.
+        Its source code can be found <a href='https://github.com/harrel56/json-schema'>here</a> and it's also accessible
+        on <a href='https://mvnrepository.com/artifact/dev.harrel/json-schema'>Maven Central</a>.
+        You can check how it compares to other implementations on <a
+        href='https://bowtie-json-schema.github.io/bowtie'>Bowtie</a>. If anything seems to not work right, please
+        report an <a href='https://github.com/harrel56/json-schema/issues'>issue</a>, it would be really appreciated.
+      </p>
+      <div className='editors-wrapper'>
+        <div className='editor-container'>
+          <h2>Schema</h2>
+          <ReactCodeMirror
+            extensions={extensions}
+            height='500px'
+            value={schema}
+            onChange={val => setSchema(val)}
+          />
+        </div>
+        <div className='editor-container'>
+          <h2>Instance</h2>
+          <ReactCodeMirror
+            extensions={extensions}
+            height='500px'
+            value={instance}
+            onChange={val => setInstance(val)}
+          />
+        </div>
+      </div>
+      <div className='buttons'>
+        <Button disabled={loading} onClick={onClick}>Validate</Button>
+        {validationResponse}
+      </div>
+      <div className='editor-container'>
+        <h2>Output</h2>
+        <ReactCodeMirror
+          extensions={[json()]}
+          height='500px'
+          value={output}
+          readOnly={true}
+        />
+      </div>
+    </>
+  )
 }
 
 export default JsonSchemaPlayground
