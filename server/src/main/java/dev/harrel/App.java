ed0c162a41e03b10897385ed6d43c23d23bcc0c1
Harrel <marcin.slowik95@gmail.com>
2023-08-11 22:14:37 +0200

    use tinelog instead of logback, update javalin

--- server/src/main/java/dev/harrel/App.java
+++ server/src/main/java/dev/harrel/App.java
@@ -1,21 +1,14 @@
 package dev.harrel;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
 import io.javalin.Javalin;
 import io.javalin.config.JavalinConfig;
 
 import java.util.Map;
 import java.util.function.Consumer;
 
-import static ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME;
-
 public class App {
 
     public static void main(String[] args) {
-        Logger root = (Logger) org.slf4j.LoggerFactory.getLogger(ROOT_LOGGER_NAME);
-        root.setLevel(Level.INFO);
-
         String version = System.getenv().getOrDefault("TAG", "unspecified");
         Consumer<JavalinConfig> configConsumer = config -> {
             config.spaRoot.addFile("/", "/web/index.html");
