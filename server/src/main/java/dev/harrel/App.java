965d429e4ff6cef8f123f6379e090e45e66b1e7f
harrel <marcin.slowik95@gmail.com>
2026-02-16 22:05:40 +0100

    cleanup

--- server/src/main/java/dev/harrel/App.java
+++ server/src/main/java/dev/harrel/App.java
@@ -7,27 +7,20 @@ import io.avaje.jex.staticcontent.StaticContent;
 public class App {
 
     static void main() {
-        StaticContent webRoot =
-                StaticContent.ofClassPath("/web/index.html")
-                        .route("/*")
+        StaticContent robots =
+                StaticContent.ofClassPath("/robots/robots.txt")
+                        .route("/robots.txt")
                         .build();
         StaticContent webBundle =
                 StaticContent.ofClassPath("/web")
                         .route("/*")
-                        .directoryIndex("files.index")
-                        .preCompress()
+                        .directoryIndex("index.html")
                         .putResponseHeader("Cache-Control", "max-age=86400")
-                        .build();
-        StaticContent robots =
-                StaticContent.ofClassPath("/robots/robots.txt")
-                        .route("/robots.txt")
-                        .putMimeTypeMapping("txt", "application/javascript")
                         .build();
 
         Jex.create()
                 .plugin(robots)
                 .plugin(webBundle)
-                .plugin(webRoot)
                 .jsonService(new Jackson3JsonService())
                 .get("/api/version", new VersionHandler())
                 .post("/api/json-validate", new ValidationHandler())
